aba2e9e7e8770079741ec8e86da916989467b6d7
pepsi <550822730@qq.com>
2019-07-09 18:45:37 +0800

    页面完成

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -2,9 +2,15 @@ $(function() {
 	AOS.init({
 		duration: 1200
 	});
+	var brandNavType=0;
 	var videoArr = ['http://vjs.zencdn.net/v/oceans.mp4', 'http://www.jplayer.org/video/m4v/Incredibles_Teaser.m4v',
 		'https://blz-videos.nosdn.127.net/1/OverWatch/AnimatedShots/Overwatch_AnimatedShot_Soldier76_Hero.mp4'
 	]
+
+	/* ----- */
+
+
+
 	//产品图移入移除
 	$("#product").on("mouseenter", ".product-box", function() {
 		$(this).find(".product-text").show();
@@ -12,6 +18,22 @@ $(function() {
 	$("#product").on("mouseleave", ".product-box", function() {
 		$(this).find(".product-text").hide();
 	})
+
+	/* ------- */
+
+
+
+	//产品图移入移除
+	$(".dropdown-menu").on("mouseenter", "a", function() {
+		$(this).css("color", "#67b8e9");
+	})
+	$(".dropdown-menu").on("mouseleave", "a", function() {
+		$(this).css("color", "#7b7e8a");
+	})
+
+	/* -------- */
+
+
 
 	// 视频点击播放
 	$("#homeVideo").on("click", ".home-video-mode-img", function() {
@@ -42,6 +64,9 @@ $(function() {
 		});
 	})
 
+	/* ------ */
+
+
 	/* 
 	 *底部图片效果
 	 */
@@ -57,4 +82,63 @@ $(function() {
 	$("#youtubeHover").on("mouseleave", function() {
 		$(this).find("img").attr("src", "static/images/footer_youtube_normal.png");
 	})
+
+	/* -------- */
+
+	/* 回到顶部 */
+	$(".goTop").on("mouseenter", function() {
+		$(this).find("img").attr("src", "static/images/back_hover.png");
+	})
+	$(".goTop").on("mouseleave", function() {
+		$(this).find("img").attr("src", "static/images/back_normal.png");
+	})
+	$('.goTop').click(function() {
+		$('html , body').animate({
+			scrollTop: 0
+		}, 'slow');
+	});
+	var win = $(window); //得到窗口对象
+	var selector = $(".goTop"); //获取导航对象
+	var height = 1000; //设置距离顶部偏移量
+	win.scroll(function() {
+		if (win.scrollTop() >= height) {
+			selector.slideDown(800).css('display', 'block');
+		} else {
+			selector.slideUp(800);
+		}
+	})
+	/* ------ */
+
+	/*tab切换*/
+	brandNavType = GetQueryString("type");
+	barndClick(brandNavType)
+	function barndClick(type){
+		$("#tabBox>div").hide().eq(type).show();
+		$(".nav-tabs li").eq(type).addClass("active").siblings().removeClass("active"); 
+	}
+	$("#nav-tabs li").on("click", function(){
+		var id = $(this).data("type");
+		if(id == 0){
+			$("#brandtextP").html("ABOUT alilo");
+			$("#brandtextH3").html("")
+		}else if(id ==1){
+			$("#brandtextP").html("More about");
+			$("#brandtextH3").html("GROWTH HISTORY")
+		}else if(id ==2){
+			$("#brandtextP").html("granted");
+			$("#brandtextH3").html("PRIZES AND AWARDS")
+		}
+		$("#tabBox>div").hide().eq(id).show();
+	})
+
+	/* ----- */
+	
+	
+	/*获取url参数 */
+	function GetQueryString(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) return unescape(r[2]);
+		return null;
+	}
 })
